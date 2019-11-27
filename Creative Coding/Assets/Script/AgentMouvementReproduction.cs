3329a0a806e8f869a35a69fb916cc1cb95e99eee
Endeavolt <49842893+Endeavolt@users.noreply.github.com>
2019-11-27 15:33:12 +0100

    Commit Reproduction first Version;

---
+++ Creative Coding/Assets/Script/AgentMouvementReproduction.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMouvementReproduction : MonoBehaviour
+{
+    AgentReproduction agentReproduction;
+    private Transform partenaire;
+    NavMeshAgent meshAgent;
+    private GameObject agentSpawn;
+    private GameObject parentSpawner;
+
+    public AgentCaste agentCasteCurrent;
+    public AgentCaste partenaireCaste;
+
+    [Range(0, 100)] public int changeToGiveSuperiorClass = 50;
+    [Range(0, 100)] public int changeToGiveInferiorClass = 50;
+
+    private ManagerAgent managerAgent;
+
+    void Start()
+    {
+        managerAgent = FindObjectOfType<ManagerAgent>();
+        agentSpawn = managerAgent.agentSpawn;
+        meshAgent = GetComponent<NavMeshAgent>();
+        agentReproduction = GetComponent<AgentReproduction>();
+        agentCasteCurrent = GetComponent<AgentCaste>();
+        partenaireCaste = partenaire.GetComponent<AgentCaste>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agentReproduction.reproduction == AgentReproduction.StateReproduction.InReproduction)
+        {
+            partenaire = agentReproduction.parternaireReproduction.transform;
+            float distance = Vector3.Distance(transform.position, partenaire.position);
+            meshAgent.SetDestination(partenaire.position);
+            if (distance < 3f)
+            {
+                meshAgent.Stop();
+                if (agentReproduction.asker)
+                {
+                    InstantiateAgent();
+                    agentReproduction.reproduction = AgentReproduction.StateReproduction.Work;
+                }
+            }
+        }
+    }
+
+    public void InstantiateAgent()
+    {
+
+        GameObject newAgent = Instantiate(agentSpawn, transform.position, Quaternion.identity, parentSpawner.transform);
+        AgentCaste agentCaste = newAgent.GetComponent<AgentCaste>();
+        int chance = Random.Range(0, 100);
+        if(agentCasteCurrent.CurrentCaste == partenaireCaste.CurrentCaste)
+        {
+            agentCaste.CurrentCaste = agentCasteCurrent.CurrentCaste;
+        }
+        if (agentCasteCurrent.CurrentCaste > partenaireCaste.CurrentCaste)
+        {
+            if (chance < changeToGiveSuperiorClass)
+            {
+                agentCaste.CurrentCaste = agentCasteCurrent.CurrentCaste;
+            }
+            else
+            {
+                agentCaste.CurrentCaste = partenaireCaste.CurrentCaste;
+            }
+
+        }
+        if(agentCasteCurrent.CurrentCaste < partenaireCaste.CurrentCaste)
+        {
+            if (chance < changeToGiveInferiorClass)
+            {
+                agentCaste.CurrentCaste = agentCasteCurrent.CurrentCaste;
+            }
+            else
+            {
+                agentCaste.CurrentCaste = partenaireCaste.CurrentCaste;
+            }
+        }
+        
+        AgentMouvementReproduction reproductionAgent = newAgent.GetComponent<AgentMouvementReproduction>();
+        reproductionAgent.agentSpawn = managerAgent.agentSpawn;
+        reproductionAgent.parentSpawner = parentSpawner;
+    }
+}
