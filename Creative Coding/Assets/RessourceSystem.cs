75313ae28da0ba841eec80a4d0b738adf5449202
Guilleade <titouan.guehard@gmail.com>
2019-11-27 14:05:11 +0100

    City

--- Creative Coding/Assets/RessourceSystem.cs
+++ Creative Coding/Assets/RessourceSystem.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class RessourceSystem : MonoBehaviour
 {
-    public int currentPopul;
-    public int religionPopul;
-    public int protectionPopul;
-    public int marchandPopul;
-    public int cultivateurpopul;
+    public float currentRadius = 1f;
+    public int currentPopul = 0;
+    public int religionPopul = 2;
+    public int protectionPopul = 2;
+    public int marchandPopul = 2;
+    public int cultivateurpopul = 4;
     int maxReligion = 0;
     int maxProtection = 0;
     int maxMarchand = 0;
@@ -29,11 +30,19 @@ public class RessourceSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentRadius = currentPopul / 10;
         currentPopul = religionPopul + protectionPopul + marchandPopul + cultivateurpopul;
         cultiText.text = ("" + cultivateurpopul + " /" + (cultivateurpopul * 100 / currentPopul) + "%");
         marchandText.text = (""+ marchandPopul + " /" + (marchandPopul * 100 / currentPopul) + "%");
         protecteurText.text = (""+ protectionPopul + " /" + (protectionPopul * 100 / currentPopul) + "%");
         religionText.text = ("" + religionPopul + " /" + (religionPopul * 100 / currentPopul) + "%");
         totalPopul.text = ("" + currentPopul);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 0.4f);
+        Gizmos.DrawSphere(transform.position, currentRadius);
+        Gizmos.DrawWireSphere(transform.position, currentRadius);
     }
 }
