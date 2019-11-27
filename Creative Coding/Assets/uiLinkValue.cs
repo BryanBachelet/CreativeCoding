bccb2cfe441c79152e2eace631c68b3824d8b56f
Guilleade <titouan.guehard@gmail.com>
2019-11-27 11:41:27 +0100

    Probuilder & time system

---
+++ Creative Coding/Assets/uiLinkValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiLinkValue : MonoBehaviour
+{
+    public Text myTextTempsEcoule;
+    public Text myTextCalendar;
+    public TimeInGame myTIG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTextTempsEcoule.text = myTIG.tempsEcoule.ToString("F1");
+        myTextCalendar.text = ("" + myTIG.day + " Jour /"+ myTIG.week + " Semaine /" + myTIG.month + " Mois /" + myTIG.year + " Année");
+    }
+}
---
+++ Creative Coding/Readme.txt
@@ -0,0 +1,2 @@
+Current : 1 minutes IRL = 840 jours IG
+			soit 2.3 années
