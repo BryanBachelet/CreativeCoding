2cf82ec5c051da9926534a78912532c6fc4888ee
Guilleade <titoug56@gmail.com>
2019-12-10 10:07:25 +0100

    lotathing

---
+++ Creative Coding/Assets/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public GameObject myCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            myCube.transform.Rotate(myCube.transform.right, 15 * Time.deltaTime);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                myCube.transform.Rotate(-myCube.transform.right, 15 * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            myCube.transform.Rotate(myCube.transform.up, 15 * Time.deltaTime);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                myCube.transform.Rotate(-myCube.transform.up, 15 * Time.deltaTime);
+            }
+        }
+    }
+
+}
---
+++ Creative Coding/Assets/GenerateTexture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTexture : MonoBehaviour
+{
+    public int CameraWidth;
+    public int CameraHeaight;
+
+    public Texture2D myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraWidth = Camera.main.pixelWidth;
+        CameraHeaight = Camera.main.pixelHeight;
+        myText = new Texture2D(CameraWidth, CameraHeaight, TextureFormat.ARGB32, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateText()
+    {
+        //olor[] = getpi
+    }
+}
